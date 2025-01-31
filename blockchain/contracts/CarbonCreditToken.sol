85463b58e9d4bd561b8cbbe5df0781cea1093d65
Chyna <w.angoye@alustudent.com>
2025-01-31 20:43:49 +0300

    add token contract

--- blockchain/contracts/CarbonCreditToken.sol
+++ blockchain/contracts/CarbonCreditToken.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract CarbonCreditToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("CarbonCreditToken", "CCT") {
+contract CarbonCreditToken is ERC20, ERC20Burnable, Ownable {
+    constructor(uint256 initialSupply) ERC20("CarbonCreditToken", "CCT") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
     }
 
