623e5b522e46886aa0ff5421bf047984cbd7f954
Chyna <w.angoye@alustudent.com>
2025-01-31 20:21:09 +0300

    add token contract

---
+++ blockchain/contracts/CarbonCreditToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CarbonCreditToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("CarbonCreditToken", "CCT") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function retire(uint256 amount) external {
+        require(amount > 0, "Amount must be greater than zero");
+        _burn(msg.sender, amount);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
