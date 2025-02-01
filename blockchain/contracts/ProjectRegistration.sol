f7c2b40e6f929c116351d7c0075e543622846802
Chyna <w.angoye@alustudent.com>
2025-02-01 22:58:02 +0300

    modify for better voters' handling

--- blockchain/contracts/ProjectRegistration.sol
+++ blockchain/contracts/ProjectRegistration.sol
@@ -17,11 +17,18 @@ contract ProjectRegistration {
 
     CarbonCreditToken public cct;
     address public admin;
-    uint256 public totalVoteWeight;
-
     mapping(uint256 => Project) public projects;
     mapping(uint256 => mapping(address => bool)) public hasVoted;
     uint256 public projectCount;
+    
+    // Track eligible voters
+    mapping(address => bool) public eligibleVoters;
+    address[] public voterList;
+    
+    event ProjectSubmitted(uint256 indexed projectId, string name, address owner);
+    event VoteCast(uint256 indexed projectId, address voter, uint256 weight);
+    event VoterAdded(address voter);
+    event ProjectRegistered(uint256 indexed projectId);
 
     /**
      * @notice Initializes the contract with the CarbonCreditToken address.
@@ -39,7 +46,22 @@ contract ProjectRegistration {
     function submitProject(string calldata _name) external {
         require(uint256(cct.roles(msg.sender)) == 2, "Only project owners can submit projects");
         projects[projectCount] = Project(_name, msg.sender, 0, false);
+        emit ProjectSubmitted(projectCount, _name, msg.sender);
         projectCount++;
+    }
+
+    /**
+     * @notice Adds an address to the list of eligible voters when they receive the buyer role.
+     * @param voter Address to be added as an eligible voter.
+     */
+    function addEligibleVoter(address voter) external {
+        require(msg.sender == address(cct), "Only CCT contract can add voters");
+        require(!eligibleVoters[voter], "Already eligible");
+        require(uint256(cct.roles(voter)) == 3, "Must be a buyer");
+        
+        eligibleVoters[voter] = true;
+        voterList.push(voter);
+        emit VoterAdded(voter);
     }
 
     /**
@@ -47,7 +69,7 @@ contract ProjectRegistration {
      * @param projectId ID of the project to vote for.
      */
     function voteForProject(uint256 projectId) external {
-        require(uint256(cct.roles(msg.sender)) == 3, "Only buyers can vote");
+        require(eligibleVoters[msg.sender], "Not an eligible voter");
         require(projectId < projectCount, "Invalid project ID");
         require(!hasVoted[projectId][msg.sender], "Already voted");
 
@@ -55,8 +77,8 @@ contract ProjectRegistration {
         require(voterBalance > 0, "Must hold CCT to vote");
 
         projects[projectId].voteWeight += voterBalance;
-        totalVoteWeight += voterBalance;
         hasVoted[projectId][msg.sender] = true;
+        emit VoteCast(projectId, msg.sender, voterBalance);
     }
 
     /**
@@ -66,10 +88,52 @@ contract ProjectRegistration {
     function finalizeProject(uint256 projectId) external {
         require(projectId < projectCount, "Invalid project ID");
         Project storage project = projects[projectId];
-
+        
         require(!project.registered, "Already registered");
-        require(project.voteWeight > totalVoteWeight / 2, "Not enough votes");
+        
+        uint256 requiredVotes = getTotalEligibleVotes() / 2;
+        require(project.voteWeight > requiredVotes, "Not enough votes");
 
         project.registered = true;
+        emit ProjectRegistered(projectId);
+    }
+    
+    /**
+     * @notice Calculates the total voting power of all eligible voters.
+     * @return Total number of tokens held by eligible voters.
+     */
+    function getTotalEligibleVotes() public view returns (uint256) {
+        uint256 totalVotes = 0;
+        for (uint i = 0; i < voterList.length; i++) {
+            totalVotes += cct.balanceOf(voterList[i]);
+        }
+        return totalVotes;
+    }
+
+    /**
+     * @notice Gets the number of eligible voters.
+     * @return Number of eligible voters.
+     */
+    function getEligibleVoterCount() external view returns (uint256) {
+        return voterList.length;
+    }
+
+    /**
+     * @notice Checks if an address is an eligible voter.
+     * @param voter Address to check.
+     * @return Whether the address is an eligible voter.
+     */
+    function isEligibleVoter(address voter) external view returns (bool) {
+        return eligibleVoters[voter];
+    }
+
+    /**
+     * @notice Gets whether an address has voted for a project.
+     * @param projectId Project ID to check.
+     * @param voter Address to check.
+     * @return Whether the address has voted for the project.
+     */
+    function hasAddressVoted(uint256 projectId, address voter) external view returns (bool) {
+        return hasVoted[projectId][voter];
     }
 }
