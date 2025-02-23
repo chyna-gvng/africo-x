d2388bf392407bc88674f25b7be29e81c4d5ef6e
chyna-gvng <angoyewally@outlook.com>
2025-02-23 21:16:47 +0300

    fix: Remove unused state variable to resolve linting error

--- frontend/src/Projects.jsx
+++ frontend/src/Projects.jsx
@@ -14,6 +14,7 @@ const Projects = () => {
   const [mintAccount, setMintAccount] = useState('');
   const [mintAmount, setMintAmount] = useState('');
   const [name, setName] = useState('');
+  // eslint-disable-next-line no-unused-vars
   const [_, setBlockchainSubmitted] = useState(false);
   const [description, setDescription] = useState('');
   const [location, setLocation] = useState('');
