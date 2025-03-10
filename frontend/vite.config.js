70ef7e2942e618ab7a35a91d4711c43f8dd545e4
chyna-gvng <angoyewally@outlook.com>
2025-03-10 20:11:54 +0300

    feat: Configure reverse proxy and update API URL for Docker setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['africox.angoyewally.dev']
-  }
+  // server: {
+  //   allowedHosts: ['africox.angoyewally.dev']
+  // }
 })
